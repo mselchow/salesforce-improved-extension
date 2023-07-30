13b87ef9bdb86ceb2f8b18e4b501c8bc2b96df25
Michael Selchow <mselchow@pm.me>
2023-07-29 20:38:47 -0500

    Add comments to CSS

--- src/scripts/flowDebugUI.js
+++ src/scripts/flowDebugUI.js
@@ -1,6 +1,7 @@
 import addGlobalStyle from "../lib/addGlobalStyle";
 
 addGlobalStyle(`
+  /* allow record selection combo box to overflow container */
   .slds-modal__content {
     overflow: visible !important;
     overflow-y: visible !important;
