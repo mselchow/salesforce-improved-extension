39b3b8a25397a5947df9520028c114f3bfeeedc2
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2023-08-07 17:38:33 -0500

    Replace Mantine with shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,36 @@
-import {
-  MantineProvider,
-  Text,
-  Container,
-  Center,
-  Space,
-  Button,
-  Stack,
-  Image,
-} from "@mantine/core";
+import { Button } from "@/components/ui/button";
 import icon from "/icons/icon-48.png";
 
 export default function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container sx={{ width: "300px", height: "200px" }}>
-        <Space h="xl" />
-        <Center>
-          <Stack>
-            <Center>
-              <Image src={icon} width={50} />
-            </Center>
-            <Text size="xl" weight={700}>
-              Salesforce Improved
-            </Text>
-
-            <Button>Settings</Button>
-          </Stack>
-        </Center>
-        <Space h="xl" />
-      </Container>
-    </MantineProvider>
+    <div className="flex flex-col border-foreground border-2 w-80">
+      <div className="flex flex-col items-center gap-5 py-10">
+        <img src={icon} className="w-50 h-50" />
+        <span className="text-2xl font-bold text-center">
+          Salesforce Improved
+        </span>
+        <Button
+          className="mt-5"
+          onClick={() => {
+            if (chrome.runtime.openOptionsPage) {
+              chrome.runtime.openOptionsPage();
+            } else {
+              window.open(chrome.runtime.getURL("options.html"));
+            }
+          }}
+        >
+          Settings
+        </Button>
+      </div>
+      <div className="flex flex-col gap-1 items-center pb-2 text-xs text-muted-foreground">
+        <a
+          href="https://github.com/mselchow/salesforce-improved-extension"
+          className="underline"
+        >
+          GitHub
+        </a>
+        <span className="">&copy; Michael Selchow</span>
+      </div>
+    </div>
   );
 }
---
+++ src/options/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Options from "./options";
+
+ReactDOM.createRoot(document.getElementById("__root")!).render(
+  <React.StrictMode>
+    <Options />
+  </React.StrictMode>
+);
