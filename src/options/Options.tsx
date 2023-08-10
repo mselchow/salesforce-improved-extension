31915f4e34dd8b1dd365c1aa88960bfe818d361e
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2023-08-09 21:42:53 -0500

    Set page spacing

--- src/options/Options.tsx
+++ src/options/Options.tsx
@@ -2,8 +2,8 @@ import OptionsForm from "@/options/OptionsForm";
 
 export default function Options() {
   return (
-    <div className="flex flex-col">
-      <h1 className="text-2xl font-bold mb-4">Options</h1>
+    <div className="flex flex-col container py-[2rem] max-w-md">
+      <h1 className="text-2xl font-bold mb-4">Salesforce Improved - Options</h1>
       <OptionsForm />
     </div>
   );
