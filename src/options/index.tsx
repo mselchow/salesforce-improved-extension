75aaad0bc8385ed0209b36da2fdfa235710cbc25
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2023-08-09 21:42:40 -0500

    Add Toaster to options page

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,10 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import "@/styles/globals.css";
+import { Toaster } from "@/components/ui/toaster";
+
 import Options from "./Options";
 
 ReactDOM.createRoot(document.getElementById("__root")!).render(
   <React.StrictMode>
     <Options />
+    <Toaster />
   </React.StrictMode>
 );
