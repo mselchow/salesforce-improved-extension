bc2175906a75d0ced8713353370813f7aa746796
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2023-08-11 08:49:54 -0500

    Add 'coming soon' message to options page

--- src/options/index.tsx
+++ src/options/index.tsx
@@ -2,13 +2,11 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import "@/styles/globals.css";
-import { Toaster } from "@/components/ui/toaster";
 
 import Options from "./Options";
 
 ReactDOM.createRoot(document.getElementById("__root")!).render(
   <React.StrictMode>
     <Options />
-    <Toaster />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
