c0dd0df91dc8cd4a7364486dffca7272eb53092e
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2023-08-21 12:05:18 -0500

    Add element wait before changing CSS

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -1,9 +1,11 @@
-minimizePopups();
+import waitForElement from "@/lib/waitForElement";
+
+const _POPUP_SELECTOR = "div.slds-docked_container.forceDockingPanel.DOCKED";
+
+waitForElement(_POPUP_SELECTOR, minimizePopups);
 
 function minimizePopups() {
-  const popups = document.querySelectorAll(
-    "div.slds-docked_container.forceDockingPanel.DOCKED"
-  );
+  const popups = document.querySelectorAll(_POPUP_SELECTOR);
 
   if (popups) {
     popups.forEach((popup) => {
