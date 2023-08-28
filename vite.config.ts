ae026f780558566cfdab941b372a56c231747cda
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2023-08-28 08:04:38 -0500

    Update import to reflect new manifest.ts file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { crx } from "@crxjs/vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-import manifest from "./manifest.json" assert { type: "json" };
+import { manifest } from "./manifest";
 
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
