ec8a76aa3c5d2e7414f1b6ae8afbe8b8bb9c544d
Michael Selchow <6621409+mselchow@users.noreply.github.com>
2024-02-08 16:40:14 -0600

    Update pop-up selector

--- src/scripts/general.js
+++ src/scripts/general.js
@@ -1,15 +1,21 @@
 import waitForElement from "@/lib/waitForElement";
 
 const _POPUP_SELECTOR = "div.slds-docked_container.forceDockingPanel.DOCKED";
+const _POPUPCONTENT_SELECTOR = "div.slds-docked-composer.slds-is-open";
 
-waitForElement(_POPUP_SELECTOR, minimizePopups);
+waitForElement(_POPUPCONTENT_SELECTOR, minimizePopups);
 
 function minimizePopups() {
   const popups = document.querySelectorAll(_POPUP_SELECTOR);
 
   if (popups) {
     popups.forEach((popup) => {
-      popup.classList.replace("DOCKED", "MINIMIZED");
+      const popupContent = popup.querySelector(_POPUPCONTENT_SELECTOR);
+
+      if (popupContent) {
+        popup.classList.replace("DOCKED", "MINIMIZED");
+        popupContent.classList.remove("slds-is-open");
+      }
     });
   }
 }
